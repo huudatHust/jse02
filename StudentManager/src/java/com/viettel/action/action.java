46c0fb3cc0702a8b3f001b92dc0098f25a611b91
huudatHust <datk571102@gmail.com>
2015-07-11 14:31:47 +0700

    tao project

---
+++ StudentManager/src/java/com/viettel/action/action.java
@@ -0,0 +1,13 @@
+package com.viettel.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ *
+ * @author TRAN HUU DAT
+ */
+public class action extends ActionSupport{
+    public String execute(){
+        return SUCCESS;
+    }
+}
